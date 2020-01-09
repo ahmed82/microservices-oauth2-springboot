616400802defc59d0432b9c23f28b487c80782c7
1426391 <ahmed.al-salih@delhaize.com>
2020-01-09 15:21:21 -0500

    create Authoraization server

---
+++ src/test/java/com/atr/authserver/Oauth2authServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atr.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2authServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
